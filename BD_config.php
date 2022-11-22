9e147a93f748c09c733027912a427640b298c475
AxelRUI <axeluriel@gmail.com>
2022-11-21 19:34:32 -0600

    Update BD_config.php

--- BD_config.php
+++ BD_config.php
@@ -4,10 +4,10 @@
 	$server="0.0.0.0";
 
 	// nombre de username de la base de datos
-	 $db_user = "root";
+	 $db_user = "admin";
 	 
 	// password
-	 $db_pass = "";
+	 $db_pass = "Xnf8IJMr5NNm";
 
 	 // Nombre de la base de datos.
 	$database = "tutoriasbd";
